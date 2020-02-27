a5bfc643c510defcf1154516407abc66211c07e1
David steinhafel <davidsteinhafel@outlook.com>
2020-02-26 18:55:19 -0600

    lots more testing and moving of displays around my js to get arrays to display in a proper order. result = program functions properly but when displaying arrays that haven been looped thru and changed into new arrays and then called by indices refering to past indices they dont match up.

--- Simulation.js
+++ Simulation.js
@@ -3,91 +3,105 @@
 
 let roundCounter = 0;
 let numPlayers = 10;
-let player = {name:"player", roundScore: 0}
+let player = { name: "player", roundScore: 0 }
 let players = [
-    {name:"player1", roundScore: 0},
-    {name:"player2", roundScore: 0},
-    {name:"player3", roundScore: 0},
-    {name:"player4", roundScore: 0},
-    {name:"player5", roundScore: 0},
-    {name:"player6", roundScore: 0},
-    {name:"player7", roundScore: 0},
-    {name:"player8", roundScore: 0},
-    {name:"player9", roundScore: 0},
-    {name:"player10", roundScore: 0}
+    { name: "player1", roundScore: 0 },
+    { name: "player2", roundScore: 0 },
+    { name: "player3", roundScore: 0 },
+    { name: "player4", roundScore: 0 },
+    { name: "player5", roundScore: 0 },
+    { name: "player6", roundScore: 0 },
+    { name: "player7", roundScore: 0 },
+    { name: "player8", roundScore: 0 },
+    { name: "player9", roundScore: 0 },
+    { name: "player10", roundScore: 0 }
 ];
 
-function rollDice(min, max){
-   return Math.floor(Math.random() * (max - min + 1)) + min; 
+function rollDice(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function rollDiceSet(){
-    let die1 = rollDice(1,5);
-    let die2 = rollDice(1,9);
-    let die3 = rollDice(1,11);
-    let die4 = rollDice(1,12);
-    let die5 = rollDice(1,21);
+function rollDiceSet() {
+    let die1 = rollDice(1, 5);
+    let die2 = rollDice(1, 9);
+    let die3 = rollDice(1, 11);
+    let die4 = rollDice(1, 12);
+    let die5 = rollDice(1, 21);
     let setScore = die1 + die2 + die3 + die4 + die5;
     return setScore;
-   
-}
-function playRound(){
-       roundCounter++;
-       document.getElementById("current").innerHTML= String(roundCounter);
-       players.map(p =>p.roundScore = (rollDiceSet()));
-       document.getElementById("playerName").innerHTML =" ";
-       players.forEach(p => document.getElementById("playerName").innerHTML+= String(p.name + " "));
-       document.getElementById("roundScore").innerHTML =" ";
-       players.forEach(p => document.getElementById("roundScore").innerHTML+= String(" " + p.roundScore))
-    }
-function shootoutRound(){
-        let selectD20 = rollDice(1, 5);
-        let d20arr = [rollDice(1, 21), rollDice(1, 21), rollDice(1, 21), rollDice(1, 21)]
-        d20arr.sort(function(a, b){return a-b});
-        
-        for(let i = 0; i < (4 -selectD20); i++){
-            d20arr.shift();
-        }
-    }
-function eliminate(){
-    if(roundCounter < 4){
-        players.sort(function(a, b){return a.roundScore-b.roundScore});
-        players.shift();
-        players.shift();
 
-    }
-    else if(roundCounter > 3){
-        players.sort(function(a, b){return a.roundScore-b.roundScore});
-        players.shift()
-    }
-    else if(roundCounter > 6){
-        displayWinner();
-    }
 }
-function displayWinner(){
+function playRound() {
     roundCounter++;
-    document.getElementById("current").innerHTML=String(roundCounter);
-    let player1 = shootoutRound();
-    let player2 = shootoutRound();
+    document.getElementById("playerName").innerHTML = "";
+    document.getElementById("roundScore").innerHTML = "";
+    document.getElementById("current").innerHTML = String(roundCounter);
+    players.forEach(p => p.roundScore = (rollDiceSet()));
+    eliminate();
+    players.forEach(p => document.getElementById("playerName").innerHTML += String(p.name + " "));
+    players.forEach(p => document.getElementById("roundScore").innerHTML += String(p.name +" " + "(" + p.roundScore+ ")" + " "))
 
     
-    if (player1 > player2){
-        alert(players[0].name + " " + "Congrats on winning!")
-        document.getElementById("myDiv1").innerHTML=String(player[0].name);
-        break
-    }
-    else{
-        alert(players[0].name + " " + "Congrats on winning!")
-        document.getElementById("playerName").innerHTML=String(player[0].name);
-        break
-    }
+
+
 }
-function playRoundMaster(){
-    if(players.length > 0){
-        playRound();
-        eliminate();
+function shootoutRound() {
+    let selectD20 = rollDice(1, 5);
+    let d20arr = [rollDice(1, 21), rollDice(1, 21), rollDice(1, 21), rollDice(1, 21)]
+    d20arr.sort(function (a, b) { return a - b });
+    for (let i = 0; i < (4 - selectD20); i++) {
+        d20arr.shift();
     }
-    else{
+    return d20arr;
+    //mis-interpreted the user storie. Thought it wanted the d-4 to determine how many of the d20 rolls you get to keep. Assumed player would keep best roles.
+}
+function eliminate() {
+    if (roundCounter < 4) {
+        players.sort(function (a, b) {return a.roundScore - b.roundScore});
+        players.shift();
+        players.shift();
+        document.getElementById("elim").innerHTML = "";
+        document.getElementById("elim").innerHTML += String(players[0].name);
+        document.getElementById("elim").innerHTML += String(players[1].name);
+    }
+    else if (roundCounter == 4 || roundCounter == 5) {
+        players.sort(function (a, b) {return a.roundScore - b.roundScore});
+        players.shift()
+        document.getElementById("elim").innerHTML = "";
+        document.getElementById("elim").innerHTML += String(players[0].name);
+    }
+    else if (roundCounter > 6) {
         displayWinner();
     }
 }
-    
+function displayWinner() {
+    roundCounter++;
+    document.getElementById("current").innerHTML = String(roundCounter);
+    let player1 = players[0];
+    let player2 = players[1];
+    player1 = shootoutRound();
+    document.getElementById("roundScore").innerHTML = String(players[0].roundScore);
+    player2 = shootoutRound();
+    document.getElementById("roundScore").innerHTML = String(players[1].roundScore);
+
+
+    if (player1 > player2) {
+        alert(players[0].name + " " + "Congrats on winning!")
+        document.getElementById("playerName").innerHTML = String(players[0].name);
+    }
+    else if (player2 > player1) {
+        alert(players[0].name + " " + "Congrats on winning!")
+        document.getElementById("playerName").innerHTML = String(players[0].name);
+    }
+    else {
+        displayWinner();
+    }
+}
+function playRoundMaster() {
+    if (players.length > 1) {
+        playRound();
+
+    }
+    else {
+        displayWinner();
+    }
+}
